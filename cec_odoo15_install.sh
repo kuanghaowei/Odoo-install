cb4eeba097d8d01fa15b4c9fd1d9e3855cb54c53
kuanghaowei <66816440+kuanghaowei@users.noreply.github.com>
2022-04-20 21:56:47 +0800

    Update cec_odoo15_install.sh
    去掉 postgresql-server-dev-all ，否则按照不上

--- cec_odoo15_install.sh
+++ cec_odoo15_install.sh
@@ -72,7 +72,8 @@ sudo apt-get install unzip -y
 # Install PostgreSQL Server
 #--------------------------------------------------
 echo -e "\n---- Install PostgreSQL Server ----"
-sudo apt-get install postgresql postgresql-server-dev-all -y
+#sudo apt-get install postgresql postgresql-server-dev-all -y
+sudo apt-get install postgresql -y
 
 echo -e "\n---- Creating the ODOO PostgreSQL User  ----"
 sudo su - postgres -c "createuser -s $OE_USER" 2> /dev/null || true
