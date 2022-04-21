465d108ca40435268be0935c8ff6bebcd12bf3be
kuanghaowei <66816440+kuanghaowei@users.noreply.github.com>
2022-04-21 12:27:06 +0800

    Update cec_odoo15_install.sh
    增加库libpq-dev，否则按照requirements报错

--- cec_odoo15_install.sh
+++ cec_odoo15_install.sh
@@ -83,6 +83,8 @@ sudo su - postgres -c "createuser -s $OE_USER" 2> /dev/null || true
 #--------------------------------------------------
 echo -e "\n--- Installing Python 3 + pip3 --"
 sudo apt-get install git python3 python3-pip build-essential wget python3-dev python3-venv python3-wheel libxslt-dev libzip-dev libldap2-dev libsasl2-dev python3-setuptools node-less libpng-dev libjpeg-dev gdebi -y
+#增加库libpq-dev，否则按照requirements报错
+sudo apt-get install libpq-dev -y
 
 echo -e "\n---- Install python packages/requirements ----"
 sudo -H pip3 install -r https://github.com/odoo/odoo/raw/${OE_VERSION}/requirements.txt
